17386731247e4be106c378c5c87bad2efeed74a0
Suhail Ansari <suhail@Suhails-MacBook-Pro.local>
2018-09-18 22:44:17 -0700

    completed color button

---
+++ Color Button/scripts/script.js
@@ -0,0 +1,16 @@
+console.log('script loaded!');
+
+function mergeColor(buttonColor){
+  let boxColor = document.getElementsByClassName('color-box');
+  console.log('boxColor: ', boxColor);
+  let buttonR = buttonColor[0];
+  let buttonG = buttonColor[1];
+  let buttonB = buttonColor[2];
+  let boxR = boxColor[0];
+  let boxG = boxColor[1];
+  let boxB = boxColor[2];
+  let mergedR = 1;
+
+}
+
+mergeColor();
